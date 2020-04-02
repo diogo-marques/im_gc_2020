f583f99b97205ba6faae9c6a723e9929a1bfcf1e
Diogo Marques <diogocostamarques@hotmail.com>
2020-04-02 22:49:16 +0100

    Start API functions and phrase recognition

---
+++ GoogleCalendar/basic4Assignmetn2/AppGui/AppGui/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using mmisharp;
+using Newtonsoft.Json;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AppGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MmiCommunication mmiC;
+
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/calendar-dotnet-quickstart.json
+        //static string[] Scopes = { CalendarService.Scope.Calendar };
+        //static string ApplicationName = "Google Calendar API .NET Quickstart";
+
+
+        public MainWindow()
+        {
+            /*
+            UserCredential credentials;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credentials = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credentials,
+                ApplicationName = ApplicationName,
+            });
+            EventDateTime start = new EventDateTime()
+            {
+                DateTime = new DateTime(2020,04,03,10,00,00),
+                TimeZone = "Europe/Lisbon",
+            };
+            EventDateTime end = new EventDateTime()
+            {
+                DateTime = new DateTime(2020, 04, 03, 11, 00, 00),
+                TimeZone = "Europe/Lisbon",
+            };
+            createEvent(service, "Jantar", "Aveiro", start, end, "Jantar com o joca");
+
+            Events events = getNextEvents(service, 10);
+            Console.WriteLine("Upcoming events:");
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
+                    Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No upcoming events found.");
+            }
+            */
+            InitializeComponent();
+
+
+            mmiC = new MmiCommunication("localhost",8000, "User1", "GUI");
+            mmiC.Message += MmiC_Message;
+            mmiC.Start();
+
+        }
+
+        private void createEvent(CalendarService service, String summary, String location, EventDateTime start, EventDateTime end, String desc)
+        {
+            // Refer to the .NET quickstart on how to setup the environment:
+            // https://developers.google.com/calendar/quickstart/dotnet
+            // Change the scope to CalendarService.Scope.Calendar and delete any stored
+            // credentials.
+
+            Event newEvent = new Event()
+            {
+                Summary = summary,
+                Location = location,
+                Description = desc,
+                Start = start,
+                End = end,
+            };
+
+            String calendarId = "primary";
+            EventsResource.InsertRequest request = service.Events.Insert(newEvent, calendarId);
+            Event createdEvent = request.Execute();
+            Console.WriteLine("Event created: {0}", createdEvent.HtmlLink);
+        }
+
+        private Events getNextEvents(CalendarService service, int maxResults)
+        {
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = maxResults;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Events events = request.Execute();
+
+            return events;
+        }
+
+
+
+        private void MmiC_Message(object sender, MmiEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+            var doc = XDocument.Parse(e.Message);
+            var com = doc.Descendants("command").FirstOrDefault().Value;
+            dynamic json = JsonConvert.DeserializeObject(com);
+
+            Console.WriteLine(json);
+
+            Shape _s = null;
+            switch ((string)json.recognized[0].ToString())
+            {
+                case "SQUARE":
+                    _s = rectangle;
+                    break;
+                case "CIRCLE":
+                    _s = circle;
+                    break;
+                case "TRIANGLE":
+                    _s = triangle;
+                    break;
+            }
+
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                switch ((string)json.recognized[1].ToString())
+                {
+                    case "GREEN":
+                        _s.Fill = Brushes.Green;
+                        break;
+                    case "BLUE":
+                        _s.Fill = Brushes.Blue;
+                        break;
+                    case "RED":
+                        _s.Fill = Brushes.Red;
+                        break;
+                }
+            });
+
+
+
+        }
+    }
+}
---
+++ GoogleCalendar/readme.txt
@@ -0,0 +1,5 @@
+Passos e requisitos:
+
+1. Open the NuGet Package Manager Console
+2. Select the package source nuget.org
+3. Run the following command: Install-Package Google.Apis.Calendar.v3
